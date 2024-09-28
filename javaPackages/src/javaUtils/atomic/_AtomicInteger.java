3fbd1295a8038334f3ca00a71ffbfa0e891c9aae
Janky Lewis <jankylewis.sae.01@gmail.com>
2024-09-28 18:20:13 +0700

    Kept branch updated

---
+++ javaPackages/src/javaUtils/atomic/_AtomicInteger.java
@@ -0,0 +1,8 @@
+package javaUtils.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class _AtomicInteger {
+
+//    AtomicInteger
+}
--- javaPackages/src/javaUtils/collectionsFramework/queue/linked/_LinkedBlockingDeque.java
+++ javaPackages/src/javaUtils/collectionsFramework/queue/linked/_LinkedBlockingDeque.java
@@ -1,4 +1,33 @@
+
 package javaUtils.collectionsFramework.queue.linked;
 
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class _LinkedBlockingDeque {
+    public static void main(String[] args) {
+
+        LinkedBlockingDeque<String> deque = new LinkedBlockingDeque<>();
+
+        // Adding elements to the front
+        deque.addFirst("A");
+        deque.addFirst("B");
+
+        // Adding elements to the back
+        deque.addLast("C");
+        deque.addLast("D");
+
+        System.out.println(deque);
+
+        // Removing elements from the front
+        System.out.println(deque.removeFirst());
+        System.out.println(deque);
+        System.out.println(deque.removeFirst());
+        System.out.println(deque);
+
+        // Removing elements from the back
+        System.out.println(deque.removeLast());
+        System.out.println(deque);
+        System.out.println(deque.removeLast());
+        System.out.println(deque);
+    }
 }
